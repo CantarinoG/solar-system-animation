a4f7fe97cd16d9b241a04eb099ccbee2cb107c71
CantarinoG <guilhermecantarino1709@gmail.com>
2024-03-12 19:27:18 -0300

    feat: Add texturized sphere for galaxy background
    Implemented a texturized sphere to represent the galaxy background.
    Included a .jpg for the sphere material.

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
-let cameraZDistance = 5;
-let cameraYDistance = -20;
-let translationSpeed = 0.002;
-let radius = 15;
-let flatEarth = false;
-
 let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 let renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-let sphereGeometry = new THREE.SphereGeometry(); //0.6 e 3
+const texture = new THREE.TextureLoader().load("./assets/galaxy.jpg");
+
+let sphereGeometry = new THREE.SphereGeometry();
 let materialPlanet = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 let materialSun = new THREE.MeshBasicMaterial({ color: 0xDECB3D });
+let materialGalaxy = new THREE.MeshBasicMaterial({ map: texture, side: THREE.BackSide });
+
 
 let planet = new THREE.Mesh(sphereGeometry, materialPlanet);
-let sun = new THREE.Mesh(sphereGeometry, materialSun)
+let sun = new THREE.Mesh(sphereGeometry, materialSun);
+let galaxy = new THREE.Mesh(sphereGeometry, materialGalaxy);
 
 planet.scale.set(0.6, 0.6, 0.6);
 scene.add(planet);
 
 sun.scale.set(3, 3, 3);
 scene.add(sun);
+
+galaxy.scale.set(20, 20, 20);
+galaxy.rotation.y = Math.PI / 2;
+galaxy.rotation.x = Math.PI / 2;
+scene.add(galaxy);
 
 camera.position.z = 5;
 camera.position.y = -20;
@@ -31,8 +35,8 @@ function animate() {
     requestAnimationFrame(animate);
 
     sun.rotation.x += 0.1
-    planet.position.x = Math.cos(Date.now() * translationSpeed) * radius
-    planet.position.y = Math.sin(Date.now() * translationSpeed) * radius / 2
+    planet.position.x = Math.cos(Date.now() * 0.002) * 15
+    planet.position.y = Math.sin(Date.now() * 0.002) * 15 / 2
     planet.rotation.x += 0.1
 
     renderer.render(scene, camera);
